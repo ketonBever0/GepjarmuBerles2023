27dcce6f671af79c8812c4883f078d10a6f252d1
ketonBever0 <lacikurucz09@gmail.com>
2023-02-09 10:31:32 +0100

    form+item_container update

--- frontend/src/components/Form.jsx
+++ frontend/src/components/Form.jsx
@@ -1,19 +1,29 @@
+import '../css/form.css'
 
 const Form = () => {
     return (
         <form>
             <div className="row mt-3">
                 <div className="col">
-                    <input type="text" className="form-control" placeholder="Márka" />
+                    {/* <input type="text" className="form-control" placeholder="Márka" /> */}
+                    <select className="form-select">
+                        <option selected value={null}>Típus</option>
+                    </select>
                 </div>
                 <div className="col">
-                    <input type="text" className="form-control" placeholder="Típus" />
+                    {/* <input type="text" className="form-control" placeholder="Modell" /> */}
+                    <select className="form-select">
+                        <option selected value={null}>Modell</option>
+                    </select>
                 </div>
             </div>
 
             <div className="row mt-3">
                 <div className="col">
-                    <input type="text" className="form-control" placeholder="Járműtípus" />
+                    {/* <input type="text" className="form-control" placeholder="Járműtípus" /> */}
+                    <select className="form-select">
+                        <option selected value={null}>Járműtípus</option>
+                    </select>
                 </div>
                 <div className="col">
                     <input type="number" min={1} max={40} className="form-control" placeholder="Férőhely" />
