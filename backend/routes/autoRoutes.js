829dd28380d4743e546f705c083d1ac7cc4319ec
ricskusz <richarszab56@gmail.com>
2023-02-09 11:08:42 +0100

    Márka szerinti lekérdezés
    Márka szerinti végpont létrehozása

--- backend/routes/autoRoutes.js
+++ backend/routes/autoRoutes.js
@@ -1,11 +1,18 @@
 const express = require('express');
 const router = express.Router();
-const {getVehicles, getVehicleBrands, getVehicleBrandTypes, getVehiclePassengerSeatsCount} = require('../controllers/autoController');
+const {
+    getVehicles,
+    getVehicleBrands,
+    getVehicleBrandTypes,
+    getVehiclePassengerSeatsCount,
+    filterVehicles
+} = require('../controllers/autoController');
 
 router.get('/jarmuvek', getVehicles);
 router.get('/markak', getVehicleBrands);
 router.get('/modellek', getVehicleBrandTypes);
 router.get('/ferohelyek', getVehiclePassengerSeatsCount);
+router.get('/marka/:keresettMarka', filterVehicles);
 
 
 
