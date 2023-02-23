7f0b426ffa084d5248b1847bad8681c93539ccd6
ketonBever0 <lacikurucz09@gmail.com>
2023-02-23 21:29:28 +0100

    kosár finomítás

--- frontend/src/components/kosar/Kosar.jsx
+++ frontend/src/components/kosar/Kosar.jsx
@@ -22,44 +22,45 @@ function Kosar() {
         <div className='bg-white absolute rounded p-3 mb-3' style={{ height: "30rem", width: "20rem", position: "fixed", bottom: "6rem", right: "1rem", float: "right" }}>
             <h1>Kosár</h1>
 
-            <div className='list-group list-group-flush mb-4'>
+            <div className='list-group list-group-flush mb-4' style={{ height: "20rem", overflow: "auto" }}>
                 {
                     kosar.length > 0 ?
                         kosar.map((jarmu, index) => (
                             <div className='list-group-item'>
-                                <div key={index} className="row row-cols-3 my-2 mx-auto">
-                                    <div className="col">{jarmu.rendszam}</div>
-                                    <div className="col">{jarmu.marka}<br />{jarmu.modell}</div>
-                                    <div className="col d-flex justify-content-end"><button className='btn rounded-circle p-0' onClick={(e => { e.preventDefault(); setKosar(kosar.filter(x => x.rendszam != jarmu.rendszam)) })}><XCircleFill color='red' size={20} /></button></div>
+                                <div key={index} className="row row-cols-3 my-2">
+                                    <div className="col text-center">{jarmu.rendszam}</div>
+                                    <div className="col text-start">{jarmu.marka} {jarmu.modell}</div>
+                                    <div className="col text-end w-25"><button className='btn rounded-circle p-0 m-0' onClick={(e => { e.preventDefault(); setKosar(kosar.filter(x => x.rendszam != jarmu.rendszam)) })}><XCircleFill color='red' size={25} /></button></div>
                                 </div>
                             </div>
                         ))
                         :
-                        <div>Üres</div>
+                        <h3 className='mt-5 text-center'>Üres</h3>
                 }
-            </div
-            >
-            <button className='btn btn-danger' disabled={kosar.length == 0} onClick={e => { e.preventDefault(); setKosar([]); }}>Ürítés</button>
+            </div>
+            <div className='d-flex justify-content-between'>
+                <button className='btn btn-danger' disabled={kosar.length == 0} onClick={e => { e.preventDefault(); setKosar([]); }}>Ürítés</button>
+                {/* {kosar != null && kosar.length != 0 && <p>Elemek száma: {kosar.length}</p>} */}
+                <button className='btn btn-primary' disabled={kosar.length == 0} onClick={
+                    e => {
+                        e.preventDefault();
+                        localStorage.setItem("kosarBackup", JSON.stringify(kosar));
+                        setKosarBackup(kosar);
+                        setIsOpen(false);
+                        // console.log(JSON.parse(localStorage.getItem("kosarBackup")));
+                        setKosar([]);
 
-            <button className='btn btn-primary' disabled={kosar.length == 0} onClick={
-                e => {
-                    e.preventDefault();
-                    localStorage.setItem("kosarBackup", JSON.stringify(kosar));
-                    setKosarBackup(kosar);
-                    setIsOpen(false);
-                    // console.log(JSON.parse(localStorage.getItem("kosarBackup")));
-                    setKosar([]);
-
-                    if (token) {
-                        navigate('/checkout');
-                    } else {
-                        // Notify.tError("Ehhez be kell jelentkezni!");
-                        navigate('/login');
+                        if (token) {
+                            navigate('/checkout');
+                        } else {
+                            // Notify.tError("Ehhez be kell jelentkezni!");
+                            navigate('/login');
+                        }
                     }
-                }
-            }>Tovább</button>
+                }>Tovább</button>
+            </div>
 
-        </div >
+        </div>
     )
 }
 
